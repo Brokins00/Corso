b20a40784ad17d1024523d661bfa5e38a2fbbcd3
Brokins00 <brokins2000@gmail.com>
2024-02-05 11:19:30 +0100

    Lezione Appunti Pratici Unit 1 Settimana 3 Giorno 1

--- Front-End/UNIT-1/S3/Giorno-1/Lezione/Appunti-Pratici/assets/js/script.js
+++ Front-End/UNIT-1/S3/Giorno-1/Lezione/Appunti-Pratici/assets/js/script.js
@@ -1,5 +1,6 @@
 let test = document.getElementById('test'); // Traversing
 // let test = document.querySelector('#test'); // Di fatto è la stessa cosa scritta sopra
+let corpoPagina = document.querySelectorAll('body')[0];
 test.innerHTML = '<h1>DOM Traversing & DOM Manipulation</h1>'; // Manipulation
 
 let contenitori = document.getElementsByTagName('div');
@@ -10,9 +11,26 @@ document.getElementById('secondo').className = 'box'
 // document.getElementById('terzo').setAttribute('style', 'color: green;')
 document.querySelector('#secondo h4').setAttribute('style', 'color: black;')
 
-let box1 = document.querySelector('#secondo')
+// let box1 = document.querySelector('#secondo')
+// console.log(box1)
 
-console.log(box1)
+let boxes = document.querySelectorAll('.box')
+console.log(boxes)
+
+document.querySelector('#terzo p').style.fontWeight = 'bold';
+
+const newDiv = document.createElement('div');
+newDiv.innerText = "Sono un nuovo div creato da Javascript";
+newDiv.style.backgroundColor = 'red';
+newDiv.style.fontWeight = 'bold';
+newDiv.style.color = 'white';
+corpoPagina.appendChild(newDiv);
+
+const link = document.createElement('a');
+link.setAttribute('href', 'https://www.google.it/')
+link.setAttribute('target', '_blank');
+link.innerText = 'Link a Google';
+test.appendChild(link)
 
 // PER SELEZIONARE UN SOLO ELEMENTO HTML:
 // document.getElementById SE L'ELEMENTO HA UN ID
