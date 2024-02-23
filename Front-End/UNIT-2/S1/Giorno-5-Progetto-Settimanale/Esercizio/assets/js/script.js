30e179f6571c834e859248a3fbc8f4208420f311
Brokins00 <brokins2000@gmail.com>
2024-02-23 16:29:44 +0100

    Progetto Settimanale UNIT 2 Settimana 1 Giorno 5

---
+++ Front-End/UNIT-2/S1/Giorno-5-Progetto-Settimanale/Esercizio/assets/js/script.js
@@ -0,0 +1,52 @@
+window.addEventListener('scroll', function() {
+    const header = document.getElementsByTagName('header')[0]
+    const content = document.getElementById('trending')
+    const bottone = document.getElementById('bottone')
+
+    const headerHeight = header.offsetHeight;
+    const contentTop = content.getBoundingClientRect().top
+
+    if (contentTop <= headerHeight) {
+        header.classList.add('scrolled')
+        bottone.classList.add('bottone_cambiato')
+    } else {
+        header.classList.remove('scrolled')
+        bottone.classList.remove('bottone_cambiato')
+    }
+})
+
+const arrayNumerica = (elemento, qtty, array) => {
+    let arrayNew = [];
+    for (let i=0; i < qtty; i++) {
+        let random = Math.floor(Math.random() * elemento.length)
+        arrayNew.push(elemento[random])
+    }
+    return arrayNew
+}
+
+window.addEventListener('load', function(e) {
+    console.log('iniziato')
+    const svgElement = document.getElementById('svgM');
+    const gElementsWithOpacityZero = svgElement.querySelectorAll('g > g[opacity="1"]');
+
+    let array = arrayNumerica(gElementsWithOpacityZero, 50)
+    let arrayInvisibile = []
+    let arrayRiempita = true;
+
+    setInterval(() => {
+        if (array.length > 0 && arrayRiempita) {
+            array[0].style.opacity = 0;
+            arrayInvisibile.push(array.shift());
+            if (array.length == 0) {
+                arrayRiempita = false;
+            }
+        } else {
+            arrayInvisibile[0].style.opacity = 1;
+            array.push(arrayInvisibile.shift());
+            if (array.length == 50) {
+                array = arrayNumerica(gElementsWithOpacityZero, 50)
+                arrayRiempita = true;
+            }
+        }
+    }, 100)
+})
