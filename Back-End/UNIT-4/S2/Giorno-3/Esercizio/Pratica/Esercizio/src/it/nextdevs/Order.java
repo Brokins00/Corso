b24c92431d42f7da6858ffd8ad3d0e1cd9d45973
Fabio Brogi <brokins2000@gmail.com>
2024-04-24 17:32:41 +0200

    Esercizio UNIT 4 Settimana 2 Giorno 3

---
+++ Back-End/UNIT-4/S2/Giorno-3/Esercizio/Pratica/Esercizio/src/it/nextdevs/Order.java
@@ -0,0 +1,69 @@
+package it.nextdevs;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    private String status;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private List<Product> products;
+    private Customer customer;
+    private static List<Order> orders = new ArrayList<>();
+
+    public Order(Long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+        Order.orders.add(this);
+    }
+
+    public static List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void addProduct(Product prodotto) {
+        this.products.add(prodotto);
+    }
+}
---
+++ Back-End/UNIT-4/S2/Giorno-3/Esercizio/Pratica/Esercizio/src/it/nextdevs/Product.java
@@ -0,0 +1,35 @@
+package it.nextdevs;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String category;
+    private Double price;
+
+    public Product(Long id, String name, String category, Double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
